4d2e142ea1584edcb10d186d8395ad2a4ce28cbc
Adolfo Builes <builes.adolfo@googlemail.com>
2011-04-06 02:33:26 -0500

    adding rials app dirs and installed plugin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comunidad::Application.config.secret_token = '7a4232d156397459a1047acf74b7bab5d94972dca62c7867339100b0a9fa5c8cb776b83350f40780476051968bac561ee56d6393bf37e77c23727e393836f3b1'
