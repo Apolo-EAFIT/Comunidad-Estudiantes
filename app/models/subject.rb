2e32730f903abf74f1dd1711893b834dfc65f01a
Enrique Arango Lyons <karlysw@gmail.com>
2011-06-12 23:56:23 -0500

    some tests for subject and teacher models

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,8 +2,14 @@ class Subject < ActiveRecord::Base
   
   has_and_belongs_to_many :teachers
   
+  after_initialize :set_defaults
+  
   validates :code, :name, :presence => true
   validates :code, :uniqueness => true
   validates :credits, :numericality => { :greater_than_or_equal_to => 0, :only_integer => true } 
+  
+  def set_defaults
+    self.credits ||= 0
+  end
   
 end
--- test/unit/teacher_test.rb
+++ test/unit/teacher_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class TeacherTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should not save teacher without a first name" do
+    teacher = Teacher.new(:last_name => "Gonzalez")
+    assert !teacher.save, "Saved teacher without a first name"
+  end
+  
+  test "should not save teacher without a last name" do
+    teacher = Teacher.new(:first_name => "Juan")
+    assert !teacher.save, "Saved teacher without a last name"
+  end
+ 
 end
