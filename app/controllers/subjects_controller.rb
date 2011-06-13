f237836ffeaa31d2fe68c008ab4a864f7dcc9b59
Li Ellis Gallardo <lellisga@gmail.com>
2011-06-13 02:38:23 -0500

    fix params

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,8 +1,19 @@
 class SubjectsController < ApplicationController
   def index
+    @subjects = Subject.all
   end
 
   def new
+    @subject = Subject.new
+  end
+
+  def create
+    @subject = Subject.new(params[:subject])
+    if @subject.save
+      redirect_to :subjects
+    else
+      render :new
+    end
   end
 
   def show
